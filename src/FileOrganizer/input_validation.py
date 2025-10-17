d89b2c24e7075f581f2639b0f3d514ba4e198075
Lilou <val.lilou.rod@gmail.com>
2025-10-16 19:03:54 -0600

    Added to input_validation.py the add_flavor prompt using the inputYesNo() function. Added to input_validation.py an if-else statement: if yes the user wants flavor in their drink they get a list of flavors. Else, no flavor, no problem the user gets a message letting them know their drink of choice is being prepared.

--- src/FileOrganizer/input_validation.py
+++ src/FileOrganizer/input_validation.py
@@ -1,6 +1,5 @@
 # This program will take your favorite morning caffeinated drink order.
 # Let me know your comfort sip!
-# By using PyInputPlus to get the user's 
 
 import pyinputplus as pyip
 
@@ -8,25 +7,23 @@ import pyinputplus as pyip
 print('\nMorning! What can I get started for you?\n')
 
 #Get user's order: drink, dairy preferences, flavor 
-drink = pyip.inputMenu(['Latte', 'Cappuccino', 'Espresso', 'Mocha', 'Cafe au Lait'],
+drink = pyip.inputMenu(['Latte', 'Cappuccino', 'Mocha', 'Cafe au Lait'],
                        prompt='')
 
 #Prompt if dairy is okay
 print('\nIs dairy okay?\n')
 
-#Yes/No input validation
-
-#If yes prompt flavor
-#If no prompt dairy alternatives
-
-dairy_alt = pyip.inputMenu(['Oat', 'Almond', 'Coconut', 'Dairy Okay'],
+dairy_alt = pyip.inputMenu(['Oat', 'Almond', 'Coconut', 'Dairy Ok'],
                        prompt='')
 
 # Prompt if the user would like to add flavor
-print('\nWould you like to add a flavor?\n')
+add_flavor = pyip.inputYesNo('\nWould you like to add a flavor? (yes/no)\n')
 # If yes, prompt flavors
-add_flavor = pyip.inputMenu(['Vanilla', 'Hazelnut', 'Caramel', 'White Chocolate'],
+if add_flavor == 'yes':
+    flavor = pyip.inputMenu(['Vanilla', 'Hazelnut', 'Caramel', 'White Chocolate'],
                        prompt='')
+   
+    print(f'\nYour {flavor} {drink} will be right out!\n')
 # If no, repeat complete order to user
-
-print(f'\nYour {add_flavor} {drink} will be right out!')
+else:
+    print(f'\nSounds good! Your {drink} will be right out.\n')
