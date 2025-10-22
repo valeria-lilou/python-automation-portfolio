dbb041a59e266f077a7e18098ea7903cde8a53c3
Lilou <val.lilou.rod@gmail.com>
2025-10-21 19:48:48 -0600

    Added the file menu_data.py. Included to menu_data.py are dictionaries for the ingredients, recipies, and the retail price of the items on the menu. The price_to_make dictionary is a work in progress with the listed keys included.

---
+++ src/FileOrganizer/menu_data.py
@@ -0,0 +1,13 @@
+Ingredients = {'Milk':{'stock_oz': 128, 'cost': 5.99, 'cost_per_unit': 0.47},
+               'Espresso':{'stock_oz': 80, 'cost': 123, 'cost_per_unit': 0.65},
+               'Chocolate':{'stock_oz': 87.3, 'cost': 177.95 ,'cost_per_unit': 0.49}}
+
+Recipes = {'Latte': {'milk_oz': 8 ,'espresso_double_oz': 0.56 },
+           'Cappuccino': {'milk_oz': 8 ,'espresso_double_oz': 0.56}, 
+           'Mocha': {'milk_oz': 8 ,'espresso_double_oz': 0.56, 'chocolate_pump_oz': 0.5}}
+
+Price_To_Make = {'Latte', 'Cappuccino', 'Mocha'}
+
+Retail_Price = {'Latte': 6,
+         'Cappuccino': 5.5,
+         'Mocha': 6.75}
